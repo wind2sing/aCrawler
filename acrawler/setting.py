2fbfd75003e348524c32e28325c66d1caacf1d0b
zireael <zireael.me@gmail.com>
2019-04-28 08:59:16 +0800

    add settings in one file

---
+++ acrawler/setting.py
@@ -0,0 +1,25 @@
+REQUEST_CONFIG = {
+    'headers': {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    },
+}
+
+
+MIDDLEWARE_CONFIG = {
+    'acrawler.handlers.RequestPrepareSession': 100,
+    'acrawler.handlers.ResponseAddParser': 100,
+    'acrawler.handlers.RequestMergeConfig': 100,
+    'acrawler.handlers.RequestDelay': 100,
+    'acrawler.handlers.CrawlerStartAddon': 100,
+    'acrawler.handlers.CrawlerFinishAddon': 100
+}
+
+DOWNLOAD_DELAY = 0
+
+LOG_LEVEL = 'DEBUG'
+
+REDIS_ENABLE = False
+REDIS_START_KEY = 'acrawler:start_urls'
+REDIS_QUEUE_KEY = 'acrawler:queue'
+REDIS_DF_KEY = 'acrawler:df'
+REDIS_ADDRESS = 'redis://localhost'
