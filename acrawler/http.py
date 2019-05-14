84a412bf25151a522957051d8da2626136470b0c
zireael <zireael.me@gmail.com>
2019-05-14 21:04:21 +0800

    feat(http): add sel / callback to response; add url_str & logger

--- acrawler/http.py
+++ acrawler/http.py
@@ -5,10 +5,13 @@ import traceback
 import logging
 from yarl import URL
 from pyquery import PyQuery
+from parsel import Selector
+
 from inspect import isasyncgenfunction, isgeneratorfunction, \
     isfunction, iscoroutinefunction, ismethod
 # Typing
-from typing import Callable, List, Union, AsyncGenerator, Iterable
+
+from typing import Callable, List, Union, AsyncGenerator, Iterable, Set
 from multidict import CIMultiDictProxy
 import http
 _Function = Callable
@@ -35,6 +38,7 @@ class Request(Task):
                  callback: _Functions = None,
                  method: str = 'GET',
                  request_config: dict = None,
+                 encoding = None,
 
                  # Below are paras for parent class
                  dont_filter: bool = False,
@@ -50,11 +54,14 @@ class Request(Task):
                          )
 
         self.url = URL(url)
+        self.url_str = str(self.url)
         self.method = method
         self.callback = callback
         self.request_config = request_config if request_config else {}
+        self.encoding = encoding
         self.session = None
         self.outer_session = session
+        self.logger = logger
 
     def _fingerprint(self):
         """fingerprint for a request task.
@@ -80,7 +87,7 @@ class Request(Task):
                     cresp, meta=self.meta, callback=self.callback, request=self)
                 rt = resp
         except Exception as e:
-            logger.error(traceback.format_exc())
+            self.logger.error(traceback.format_exc())
             rt = e
         finally:
             await self.close()
@@ -98,7 +105,7 @@ class Request(Task):
 class Response(Task):
     """Response is a Task that execute parse function.
 
-    :param status_code: HTTP status code of response, e.g. 200.
+    :param status: HTTP status code of response, e.g. 200.
     :param url:
     :param cookies: HTTP cookies of response (Set-Cookie HTTP header).
     :param headers: A case-insensitive multidict proxy with HTTP headers of response.
@@ -111,7 +118,7 @@ class Response(Task):
 
     def __init__(self,
                  url: URL,
-                 status_code: int,
+                 status: int,
                  cookies: _Cookies,
                  headers: CIMultiDictProxy,
                  history: _History,
@@ -123,7 +130,8 @@ class Response(Task):
                  ):
         super().__init__(dont_filter=True)
         self.url = url
-        self.status_code = status_code
+        self.url_str = str(url)
+        self.status = status
         self.cookies = cookies
         self.headers = headers
         self.history = history
@@ -132,13 +140,16 @@ class Response(Task):
         self.meta = meta
         self.callback = callback
         self.request = request
+        self.logger = logger
+
+        self.sel:Selector = Selector(self.text)
 
         #: A pyquery instance for the response's body.
         self.doc = PyQuery(self.body)
         self.doc.make_links_absolute(str(self.request.url))
 
         #: All callback functions stores in the list.
-        self.callbacks: List[Callable] = []
+        self.callbacks: Set[Callable] = set()
         if isinstance(self.callback, Iterable):
             for cb in self.callback:
                 self.add_callback(cb)
@@ -147,8 +158,14 @@ class Response(Task):
 
     @classmethod
     async def from_ClientResponse(cls, resp, meta, callback, request):
+        body = await resp.read()
+        text = None
+        try:
+            text = await resp.text(request.encoding)
+        except Exception:
+            text = str(body)
         r = cls(
-            status_code=resp.status,
+            status=resp.status,
             url=resp.url,
             cookies=resp.cookies,
             headers=resp.headers,
@@ -156,8 +173,8 @@ class Response(Task):
             meta=meta,
             callback=callback,
             request=request,
-            body=await resp.read(),
-            text=await resp.text(),
+            body=body,
+            text=text,
         )
         return r
 
@@ -178,7 +195,13 @@ class Response(Task):
     def add_callback(self, func: _Function):
         """Add a callable function to response's :attr:`callbacks`"""
         if isfunction(func) or ismethod(func):
-            self.callbacks.append(func)
+            self.callbacks.add(func)
+    
+    def reset_callback(self):
+        self.callbacks = set()
+    
+    def delete_callback(self,func: _Function):
+        self.callbacks.discard(func)
 
     def __str__(self):
-        return f"<Task Response> <{self.status_code}> ({self.url})"
+        return f"<Task Response> <{self.status}> ({self.url})"
