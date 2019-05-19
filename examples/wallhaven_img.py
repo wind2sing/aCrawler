93c6daece8fd0492da350489cec72fcb9f510046
zireael <zireael.me@gmail.com>
2019-05-19 17:18:41 +0800

    fix(examples): update WALLHAVEN with callback

--- examples/wallhaven_img.py
+++ examples/wallhaven_img.py
@@ -1,4 +1,4 @@
-from acrawler import Crawler, Request, Response, middleware, register
+from acrawler import Crawler, Request, Response, middleware, register, callback
 from acrawler.http import FileRequest
 from acrawler.utils import open_html
 from acrawler.handlers import ResponseCheckStatus
@@ -17,47 +17,39 @@ def img_dir():
         IMG_DIR.mkdir()
     return IMG_DIR
 
-@register()
-class ChangeExt(ResponseCheckStatus):
-
-    async def on_start(self):
-        self.fdir = img_dir()
-
-    async def handle_before(self, response):
-        # we are guessing the real url of the image
-        if response.status == 404 and 'FileRequest' in response.request.families:
-            old_url = response.request.url_str
-            if 'jpg' in old_url:
-                url = old_url.replace('jpg','png')
-                await self.crawler.add_task(FileRequest(url, meta={'_fdir': self.fdir}))
-            elif 'png' in old_url:
-                url = old_url.replace('png','jpg')
-                await self.crawler.add_task(FileRequest(url, meta={'_fdir': self.fdir}))
-        else:
-            await super().handle_before(response)
-
 
 class WHCrawler(Crawler):
     config = {
         'LOG_LEVEL': 'INFO'
-    }
-
-    middleware_config = {
-        'acrawler.handlers.ResponseCheckStatus': 0, # disable it
     }
     
     max_requests = 10
     
 
     async def start_requests(self):
-        yield Request(wh_fav(1), callback=self.parse_top_search)
+        yield Request(wh_top(1), callback=self.parse_top_search)
 
+    def gen_req(self, url):
+        return FileRequest(url, meta={'_fdir': self.fdir}, family='wh' ,status_allowed=[404])
 
     async def parse_top_search(self, response:Response):
         pids = response.sel.css('#thumbs li a.preview::attr(href)').re('.*wallpaper/(\d+).*')
-        fdir = img_dir()
+        self.fdir = img_dir()
         for pid in pids:
-            yield FileRequest('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{}.jpg'.format(pid), meta={'_fdir': fdir})
+            yield self.gen_req('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{}.jpg'.format(pid))
+
+    
+    @callback('wh')
+    async def redirect_png(self, response:Response):
+        if response.status==404:
+            old_url = response.request.url_str
+            if 'jpg' in old_url:
+                url = old_url.replace('jpg','png')
+                yield self.gen_req(url)
+            elif 'png' in old_url:
+                url = old_url.replace('png','jpg')
+                yield self.gen_req(url)
+
 
 
 
