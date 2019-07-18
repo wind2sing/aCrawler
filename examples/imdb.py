f5ca4f37fdd1f92d3e914640b75a25f245d8bc91
zireael <zireael.me@gmail.com>
2019-07-18 23:07:10 +0800

    docs: update examples

--- examples/imdb.py
+++ examples/imdb.py
@@ -1,19 +1,18 @@
-from acrawler import Crawler, Request, Response, ParselItem, Processors, Handler, register, get_logger
-from pprint import pprint
+from acrawler import Crawler, Request, ParselItem, Handler, register, get_logger
 
 
 def process_time(value):
-    # a self-defined field process function
+    # a self-defined field processing function
     # process time to minutes
     # '3h 1min' -> 181
     if value:
         res = 0
-        segs = value.split(' ')
+        segs = value.split(" ")
         for seg in segs:
-            if seg.endswith('min'):
-                res += int(seg.replace('min', ''))
-            elif seg.endswith('h'):
-                res += 60*int(seg.replace('h', ''))
+            if seg.endswith("min"):
+                res += int(seg.replace("min", ""))
+            elif seg.endswith("h"):
+                res += 60 * int(seg.replace("h", ""))
         return res
     else:
         return value
@@ -21,65 +20,56 @@ def process_time(value):
 
 class MovieItem(ParselItem):
     css_rules_first = {
-        'title': 'h1::text',
-        'date': '.subtext a[href*=releaseinfo]::text',
-        'time': '.subtext time::text',
-        'rating': 'span[itemprop=ratingValue]::text',
-        'rating_count': 'span[itemprop=ratingCount]::text',
-        'metascore': '.metacriticScore span::text'
+        "title": "h1::text",
+        "date": ".subtext a[href*=releaseinfo]::text",
+        "time": ".subtext time::text",
+        "rating": "span[itemprop=ratingValue]::text",
+        "rating_count": "span[itemprop=ratingCount]::text",
+        "metascore": ".metacriticScore span::text",
     }
 
     css_rules = {
-        'genres': '.subtext a[href*=genres]::text',
-        'director': 'h4:contains(Director) ~ a[href*=name]::text',
-        'writers': 'h4:contains(Writer) ~ a[href*=name]::text',
-        'stars': 'h4:contains(Star) ~ a[href*=name]::text'
+        "genres": ".subtext a[href*=genres]::text",
+        "director": "h4:contains(Director) ~ a[href*=name]::text",
+        "writers": "h4:contains(Writer) ~ a[href*=name]::text",
+        "stars": "h4:contains(Star) ~ a[href*=name]::text",
     }
 
-    field_processors = {
-        'time': process_time
-    }
-
-    def custom_process(self, content):
-        pprint(content)
-        pass
+    field_processors = {"time": process_time}
 
 
 class IMDBCrawler(Crawler):
-    config = {
-        'LOG_LEVEL': 'INFO',
-        'MAX_REQUESTS': 6,
-        'PERSISTENT': True,
-        'PERSISTENT_NAME': 'IMDBv0.1'
-    }
+    config = {"MAX_REQUESTS": 4, "DOWNLOAD_DELAY": 1}
 
     async def start_requests(self):
-        yield Request('https://www.imdb.com/chart/moviemeter')
+        yield Request("https://www.imdb.com/chart/moviemeter", links_to_abs=True)
 
     async def parse(self, response):
-        count = 0
-        for tr in response.sel.css('.lister-list tr'):
-            link = tr.css('.titleColumn a::attr(href)').get()
+        for tr in response.sel.css(".lister-list tr"):
+            link = tr.css(".titleColumn a::attr(href)").get()
             if link:
-                yield Request(response.urljoin(link), callback=self.parse_movie)
-                count += 1
-                if count==10:
-                    break
+                yield Request(link, callback=self.parse_movie)
 
     async def parse_movie(self, response):
         url = response.url_str
-        yield MovieItem(response.sel, extra={'url': url.split('?')[0]})
+        yield MovieItem(response.sel, extra={"url": url.split("?")[0]})
 
 
 @register()
 class HorrorHandler(Handler):
-    family = 'MovieItem'
-    logger = get_logger('horrorlogger')
+    family = "MovieItem"
+    logger = get_logger("horrorlog")
 
     async def handle_after(self, item):
-        if item['genres'] and 'Horror' in item['genres']:
-            self.logger.warning(
-                "({}) is a horror movie!!!!".format(item['title']))
+        if item["genres"] and "Horror" in item["genres"]:
+            self.logger.warning("({}) is a horror movie!!!!".format(item["title"]))
+
+            yield {"singal": "Leaving...", "title": item["title"]}
+
+
+@register("DefaultItem")
+def print_item(item):
+    print(item.content)
 
 
 if __name__ == "__main__":
--- examples/quotes.py
+++ examples/quotes.py
@@ -1,53 +1,46 @@
 # Scrape quotes from http://quotes.toscrape.com/
-from acrawler import Parser, Crawler, Processors, ParselItem, get_logger, Request
-import asyncio
-logger = get_logger('quotes')
+from acrawler import get_logger
+from acrawler import Parser, Crawler, ParselItem, Request
 
 
-def get_twenty_words(value):
-    return value[:20]
+logger = get_logger("quotes")
 
 
 class QuoteItem(ParselItem):
     log = True
-    default_rules = {'type': 'quote'}
-    css_rules_first = {'author': 'small.author::text'}
-    xpath_rules_first = {'text': './/span[@class="text"]/text()'}
+    default_rules = {"type": "quote"}
+    css_rules_first = {"author": "small.author::text"}
+    xpath_rules_first = {"text": './/span[@class="text"]/text()'}
 
-    field_processors = {
-        'text': get_twenty_words,
-    }
+    field_processors = {"text": lambda s: s.strip("“")[:20]}
 
 
 class AuthorItem(ParselItem):
-    css_rules_first = {'name': 'h3.author-title::text',
-                       'born': 'span.author-born-date::text',
-                       }
+    log = True
+    default_rules = {"type": "author"}
+    css_rules_first = {
+        "name": "h3.author-title::text",
+        "born": "span.author-born-date::text",
+    }
 
 
 class QuoteCrawler(Crawler):
 
-    config = {'LOG_LEVEL': 'DEBUG',
-              'MAX_REQUESTS': 8,
-              'MAX_REQUESTS_PER_HOST': 8,
-              #   'PERSISTENT': True,
-              }
+    main_page = r"quotes.toscrape.com/page/\d+"
+    author_page = r"quotes.toscrape.com/author/.*"
+    parsers = [
+        Parser(
+            in_pattern=main_page,
+            follow_patterns=[main_page, author_page],
+            item_type=QuoteItem,
+            css_divider=".quote",
+        ),
+        Parser(in_pattern=author_page, item_type=AuthorItem),
+    ]
 
-    start_urls = ['http://quotes.toscrape.com/page/1/',
-                  'http://quotes.toscrape.com/page/3/',
-                  'http://quotes.toscrape.com/page/5/',
-                  'http://quotes.toscrape.com/page/7/']
-
-    main_page = r'quotes.toscrape.com/page/\d+'
-    author_page = r'quotes.toscrape.com/author/.*'
-    parsers = [Parser(in_pattern=main_page,
-                      follow_patterns=[main_page, author_page],
-                      item_type=QuoteItem,
-                      css_divider='.quote'
-                      ),
-               Parser(in_pattern=author_page, item_type=AuthorItem)
-               ]
+    async def start_requests(self):
+        yield Request(url="http://quotes.toscrape.com/page/1/")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     QuoteCrawler().run()
