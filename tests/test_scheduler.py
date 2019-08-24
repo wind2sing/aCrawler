bdec6d296b743d8ac227edf7e6856e8339ab8b78
zireael <zireael.me@gmail.com>
2019-08-24 08:37:40 +0800

    test: scheduler

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,14 +1,14 @@
-from acrawler.scheduler import Scheduler
-from acrawler.http import Request
 import pytest
-import asyncio
+
+from acrawler.http import Request
+from acrawler.scheduler import Scheduler
 
 
 @pytest.mark.asyncio
 async def test_queue():
     sdl = Scheduler()
-    rq1 = Request('https://www.baidu.com')
-    rq2 = Request('https://www.bing.com')
+    rq1 = Request("https://www.baidu.com")
+    rq2 = Request("https://www.bing.com")
     assert await sdl.produce(rq1)
     assert await sdl.produce(rq2)
     assert not await sdl.produce(rq1)
@@ -17,8 +17,8 @@ async def test_queue():
 @pytest.mark.asyncio
 async def test_dont_filter():
     sdl = Scheduler()
-    rq1 = Request('https://www.baidu.com')
-    rq2 = Request('https://www.bing.com', dont_filter=True)
+    rq1 = Request("https://www.baidu.com")
+    rq2 = Request("https://www.bing.com", dont_filter=True)
     assert await sdl.produce(rq1)
     assert not await sdl.produce(rq1)
     assert await sdl.produce(rq2)
@@ -28,8 +28,8 @@ async def test_dont_filter():
 @pytest.mark.asyncio
 async def test_consume():
     sdl = Scheduler()
-    rq1 = Request('https://www.baidu.com')
-    rq2 = Request('https://www.bing.com')
+    rq1 = Request("https://www.baidu.com")
+    rq2 = Request("https://www.bing.com")
     assert await sdl.produce(rq1)
     assert await sdl.produce(rq2)
     assert await sdl.consume() is rq1
