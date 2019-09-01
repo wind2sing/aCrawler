44b26e5da9cc768ffb6fb168dea0ef28a0888551
zireael <zireael.me@gmail.com>
2019-09-01 23:20:25 +0800

    style: remove commented

--- acrawler/handlers.py
+++ acrawler/handlers.py
@@ -118,33 +118,6 @@ class RequestMergeConfig(Handler):
             request.request_config["headers"] = h
 
 
-# class RequestDelay(Handler):
-#     family = "Request"
-
-#     def on_start(self):
-#         self.delay = self.crawler.config.get("DOWNLOAD_DELAY", 0)
-#         self.conf = self.crawler.config.get("DOWNLOAD_DELAY_SPECIAL_HOST", {}).copy()
-#         self.hosts = list(self.conf.keys())
-
-#     async def handle_before(self, request: _Request):
-#         origin = True
-#         target = 0
-#         for host in self.hosts:
-#             if host in request.url.host:
-#                 target += self.conf[host]
-#                 origin = False
-
-#         if origin:
-#             target = self.delay
-
-#         delay = randint(int(target * 8), (target * 12)) / 10
-#         await asyncio.sleep(delay)
-#         await self.crawler.counter.required_inc()
-
-#     async def handle_after(self, request):
-#         await self.crawler.counter.required_dec()
-
-
 # Response Part
 
 
